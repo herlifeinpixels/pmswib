d4aee91c443b806ea985d7ab6437f6f8a0f9f3b1
Hannah Wei <yw.hannah@gmail.com>
2013-02-28 03:23:12 -0500

    some todos

--- extension/popup.js
+++ extension/popup.js
@@ -1,11 +1,13 @@
 // By Hannah Wei @herlifeinpixels
 // Show recent DL on popup
 
+var displayLimit = 10;
+
 var showDownloads = {
 	// Let's do this
 	init: function() {
 		var fileList = chrome.downloads.search({
-			"limit" : 10,
+			"limit" : displayLimit,
 			"orderBy" : "-startTime"
 		}, function (fileList) {
 			//sort the thing by file type
@@ -15,7 +17,7 @@ var showDownloads = {
 	
 	// sort: function() {}
 	
-	
+	// getfileIcon: function(
 };
 
 document.addEventListener('DOMContentLoaded', function () {
